3f92303e59073fd2824f015881e7a6fe399f0ff1
Abdul Mannan <161303446+Mannan1101@users.noreply.github.com>
2025-01-14 02:34:02 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -11,10 +11,10 @@ const About = () => {
             {/* Text Section */}
             <div className="w-full h-auto  px-6 py-12 md:px-24 md:py-24 text-center md:text-left">
                 <h1 className="text-4xl md:text-6xl font-mono font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500">
-                    Hi, I am Abdul Mannan
+                    Hi I am Abdul Mannan
                 </h1>
-                <p className="text-lg md:text-xl mt-6 md:mt-9 font-serif leading-relaxed font-medium ">
-                    "Hi, I’m a passionate web developer eager to create beautiful and functional websites. I specialize in building user-friendly interfaces and seamless web experiences using modern tools like Next.js and Sanity.io. As a beginner developer, I’m always learning and excited to grow my skills, tackling new challenges with enthusiasm. Whether it's designing responsive layouts or optimizing website performance, I’m committed to delivering value through my work. Let’s connect and build something amazing together!"
+                <p className="text-lg md:text-xl mt-6 md:mt-9 font-serif leading-relaxed font-medium">
+                    Hi I am a passionate web developer eager to create beautiful and functional websites. I specialize in building user-friendly interfaces and seamless web experiences using modern tools like Next.js and Sanity.io. As a beginner developer, I am always learning and excited to grow my skills, tackling new challenges with enthusiasm. Whether it's designing responsive layouts or optimizing website performance, I am committed to delivering value through my work. Lets connect and build something amazing together
                 </p>
             </div>
 
@@ -27,19 +27,12 @@ const About = () => {
                     height={300}
                     className="rounded-[50%]  shadow-black shadow-[0px_4px_20px_0px_rgba(0,0,0,0.5)]"
                 />
+
             </div>
-
-
-
-
 
         </div>
 
-
-
-
-
-    )
+    );
 }
 
-export default About
+export default About;
